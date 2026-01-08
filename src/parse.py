a455bfd6e6d3aab3539c19d4a128641e83f6ccf6
muazalamri <muazalamri2006@gmail.com>
2026-01-08 17:37:23 +0200

    improve

--- src/parse.py
+++ src/parse.py
@@ -80,42 +80,21 @@ def only_package(line:str)->bool:
 def parse(code:str)->None:#Proto:
     text:list[str]=code.split("\n")
     line_num:int=0
-    names_def_levels:dict[str,int|tuple[int,int]]=[]
     first_ele:bool=False
     first_ele_index:int=0
     for line_num in range(len(text)):
         ##print(i)
+        if "IS" in text[line_num].split():
+            text[line_num]=text[line_num].replace('IS','"IS')+'"'
         if '{' in text[line_num]:
             first_ele = True
         if not first_ele :
             first_ele_index+=1
             continue
         else:
-            if (']' in text[line_num]):
-                text[line_num]=text[line_num].replace(']','}')
-
-            place:int=contain_list(text[line_num])
-            name_place:int|tuple[int,int]=name_start(text[line_num])
-            names_def_levels.append({
-              'index':line_num,
-              'name_place':name_place
-            })
-            if place>0:
-                if '{' not in text[line_num]:
-                    if ('"' not in text[line_num]):
-                        if ('[' in text[line_num]):
-                            text[line_num]=text[line_num].replace('[','{')
-
-                        elif ('[' not in text[line_num]):
-                            text[line_num] =text[line_num][:place] + ": ["+text[line_num][place:].replace(' ',',') + ']'
-                            if line_num!=len(text):
-                                if contain_list(text[line_num+1])>-1:
-                                    text[line_num]+=','
-                    else:
-                        text[line_num]=text[line_num][:place]+": "+text[line_num][place:]
-                if type(name_place)==tuple:
-                    text[line_num]=text[line_num][:name_place[0]]+'"'+text[line_num][name_place[0]:name_place[1]-1]+'"'+text[line_num][name_place[1]-1:]
-                if '{' in text[line_num]:text[line_num]=text[line_num].replace('{',' : {')
+            next_line:str=text[line_num+1] if line_num<len(text)-1 else ''
+            text[line_num]=adapt_line(text[line_num],next_line,line_num!=len(text))
+    text=list(map(handel_url,text))
     #print('p1:',text)
     text=write_commas(text)
     new_code:str='{'+'\n'.join(text[first_ele_index:])+'}'
@@ -125,111 +104,58 @@ def parse(code:str)->None:#Proto:
     obj=dict(json.loads(new_code))
     #print(obj)
     return obj
+
+def handel_url(line:str):
+    if "http" in line:
+        first_char:int=first_char_index(line)
+        line=line[first_char:]+'"url":'+line[:first_char]
+    return line
+
+
+def adapt_line(line:str,next_line:str,last:bool)->str:
+    if (']' in line):
+        line=line.replace(']','}')
+
+    place:int=contain_list(line)
+    name_place:int|tuple[int,int]=name_start(line)
+    if place>0:
+        if '{' not in line:
+            if ('"' not in line):
+                if ('[' in line):
+                    line=line.replace('[','{')
+
+                elif ('[' not in line):
+                    line =line[:place] + ": ["+line[place:].replace(' ',',') + ']'
+                    if last:
+                        if contain_list(next_line)>-1:
+                            line+=','
+
+            else:
+                line=line[:place]+": "+line[place:]
+        if type(name_place)==tuple:
+            line=line[:name_place[0]]+'"'+line[name_place[0]:name_place[1]-1]+'"'+line[name_place[1]-1:]
+        if '{' in line:line=line.replace('{',' : {')
+    return line
+
 def write_commas(text:list[str])->list[str]:
     prev_def_levels:list[int]=[]
     line_index:int=0
-    while line_index<len(text)-1:
-        firstCharIndex:int=first_char_index(text[line_index])
-        next_line_firstCharIndex=first_char_index(text[line_index+1])
-        if firstCharIndex>-1:
-            if firstCharIndex>=next_line_firstCharIndex:
-                print(line_index)
-                text[line_index]=text[line_index][:firstCharIndex]+',_+_+_+_+'+text[line_index][firstCharIndex:]
-            if text[line_index][firstCharIndex]=='[':
-                prev_def_levels.append(firstCharIndex)
-            if text[line_index][firstCharIndex]==']':
-                prev_def_levels.pop()
-            if text[line_index][firstCharIndex] in ['}' ,']']:
-                if len(prev_def_levels)>0:
-                    if firstCharIndex<prev_def_levels[-1]:
-                        text[line_index]=text[line_index][:prev_def_levels[0]]+','+text[line_index][prev_def_levels[0]:]
-        line_index+=1
-        
+    def_level:int=first_char_index(text[line_index])
+    prev_def_levels.append(def_level)
+
+    for line_index in range(1,len(text)-1):
+        def_level=first_char_index(text[line_index])
+        next_def_level:int=first_char_index(text[line_index+1])
+        if len(text[line_index]) > 0:
+            if def_level==next_def_level:
+                if (def_level in prev_def_levels) and (def_level>0):
+                    if text[line_index][-1]not in [',',' ']:
+                        if text[line_index+1][-1] not in ['}']:
+                            text[line_index]=text[line_index]+','
+
+                elif text[line_index][-1]not in [',',' ']:
+                    if text[line_index+1][next_def_level]=='"':
+                        if text[line_index+1][-1] not in ['}']:
+                            text[line_index]=text[line_index]+','
+        prev_def_levels.append(def_level)        
     return text
-"""        
-    proto:Proto=Proto(**dict(json.loads(str(text))))
-    return proto"""
-
-(parse("""#VRML_SIM R2025a utf8
-
-EXTERNPROTO "https://raw.githubusercontent.com/cyberbotics/webots/R2025a/projects/objects/backgrounds/protos/TexturedBackground.proto"
-EXTERNPROTO "https://raw.githubusercontent.com/cyberbotics/webots/R2025a/projects/objects/backgrounds/protos/TexturedBackgroundLight.proto"
-EXTERNPROTO "https://raw.githubusercontent.com/cyberbotics/webots/R2025a/projects/objects/floors/protos/RectangleArena.proto"
-
-WorldInfo {
-}
-Viewpoint {
-  orientation 0.27373459355434776 0.03984522123410845 -0.9609795682721047 2.8636329465408457
-  position 0.7357993402763815 0.4268963339358584 0.4381254455861484
-}
-TexturedBackground {
-}
-TexturedBackgroundLight {
-}
-RectangleArena {
-}
-Robot {
-  translation 0 0 0.1
-  children [
-    Solid {
-      translation 0 0.075 0.05
-      children [
-        HingeJoint {
-          jointParameters HingeJointParameters {
-            position 67.58400000000005
-            axis 0 1 0
-          }
-          device [
-            RotationalMotor {
-              name "motor"
-            }
-          ]
-          endPoint Solid {
-            rotation 0 1 0 4.752146928204181
-            children [
-              Solid {
-                translation 0 0.025 0.075
-                children [
-                  Shape {
-                    appearance PBRAppearance {
-                      baseColor 1 0 0
-                    }
-                    geometry Box {
-                      size 0.05 0.05 0.2
-                    }
-                  }
-                ]
-              }
-              Shape {
-                appearance PBRAppearance {
-                  baseColor 1 0 0
-                }
-                geometry Box {
-                  size 0.05 0.05 0.05
-                }
-              }
-            ]
-          }
-        }
-      ]
-      name "solid(1)"
-    }
-    Solid {
-      children [
-        Transform {
-          children [
-            DEF shape Shape {
-              appearance PBRAppearance {
-              }
-              geometry Box {
-                size 0.1 0.1 0.2
-              }
-            }
-          ]
-        }
-      ]
-    }
-  ]
-  controller "arm_hand"
-}
-"""))
